e18cb02aee5a482e210bcdcf918cc35209b33e4f
devyramadhani <devyramadhani274@gmail.com>
2023-11-03 07:45:27 +0700

    pertama commit

---
+++ kuis1.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 1) {
+      console.log(i + "-santai");
+    } else {
+      if (i % 3 === 0) {
+        console.log(i + "-I love coding");
+      } else {
+        console.log(i + "-berkualitas");
+      }
+    }
+  }
---
+++ kuis2.js
@@ -0,0 +1,19 @@
+//versi1
+console.log("\n\nVersi 1");
+for (let i = 1; i <= 10; i++) {
+  let row = '';
+  for (let j = 1; j <= 10; j++) {
+    row += j + ' ';
+  }
+  console.log(row);
+}
+
+//versi2
+console.log("\n\nVersi 2");
+for (let i = 1; i <= 10; i++) {
+    let row = '';
+    for (let j = 1; j <= 10; j++) {
+      row += '#';
+    }
+    console.log(row);
+  }
