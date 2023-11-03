efc0f77fc453a4f708b8c039c0f3b95d5ff8feb8
devyramadhani <149552637+devyramadhani@users.noreply.github.com>
2023-11-03 11:54:04 +0700

    Add files via upload

---
+++ latihan1.js
@@ -0,0 +1,15 @@
+const restaurant = {
+    name: "Resto A",
+    city: "Kota ",
+    "favorit drink": "Es Macha ",
+    "favorit food": "Ayam Spaice",
+    isvegan: true
+  };
+  
+  const name = restaurant.name;
+  
+  const favoriteDrink = restaurant["favorit drink"];
+  
+  console.log("Name:", name);
+  console.log("Favorite Drink:", favoriteDrink);
+  
